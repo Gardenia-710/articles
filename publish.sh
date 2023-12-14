5807081c2f318fb612c6feeccba29d23afe7387f
Gardenia-710 <56499607+Gardenia-710@users.noreply.github.com>
2023-12-14 14:51:06 +0900

    投稿の準備

---
+++ publish.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 1. .md形式の変更があったファイルのパスをファイル名を取得
+CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r HEAD | grep '\.md$')
+
+# 2&3. 変更があったファイルごとに指定されたコマンドを実行
+for FILE in $CHANGED_FILES; do
+  # 変更があったファイルのパスを表示
+  echo "Changed file path: $FILE"
+
+  # ファイルのパスがarticle配下ではない場合処理をスキップ
+  if [[ ! $FILE =~ ^article/ ]]; then
+    echo "Skip"
+    continue
+  fi
+
+  # .md拡張子を削除してダイル名を取得
+  FILE_NAME=$(basename $FILE .md)
+
+  # ファイル名と同じファイルがqiita/public配下に存在しない場合だけ、npx qiita newを実行
+  if [[ ! -e "qiita/public/$FILE_NAME.md" ]]; then
+    cd ./qiita
+    echo "npx qiita new $FILE_NAME"
+    npx qiita new $FILE_NAME
+    cd ../
+  fi
+
+  #  scriptを実行
+  ./node_modules/.bin/ts-node scripts/ztoq.ts "$FILE" "qiita/public/$FILE_NAME.md"
+done
+
+# GitHubにpush
+git add .
+git commit -m "[Bot] Update articles"
+git push
